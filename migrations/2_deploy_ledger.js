34a75e85200eca6824cee9befaf3d6482400b189
Anthony Akentiev <Anthony.Akentiev@gmail.com>
2018-09-08 01:55:51 +0200

    Adding Ledger contract

---
+++ migrations/2_deploy_ledger.js
@@ -0,0 +1,13 @@
+/* global artifacts */
+/* eslint no-undef: "error" */
+
+const Ledger = artifacts.require("Ledger")
+
+module.exports = function(deployer, network, accounts) {  
+  const account = accounts[0]
+  return deployer
+    .then(() => {
+      return deployer.deploy(Ledger)
+    })
+}
+
