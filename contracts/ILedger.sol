1eb24052a440709e2a46b121770e8bb976814089
Anthony Akentiev <Anthony.Akentiev@gmail.com>
2018-09-08 03:09:19 +0200

    Continue

---
+++ contracts/ILedger.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.24;
+
+contract ISideA {
+// 1 - deposit + withdraw
+	function deposit() public payable;
+	function getDepositBalance() public view returns(uint totalBalanceWei);
+	function withdraw(uint _amountWei) public;
+
+// 2 - allowances that i set to some SideB
+	// this will create _numberOfPeriods allowances
+	// if you have created allowances before for sideB -> throw
+	function allowAndDeposit(
+		address _sideB, 
+		uint _amountWei, 
+		uint _overdraftPpm, 
+		uint _interestRatePpm, 
+		uint _numberOfPeriods, 
+		uint _periodSeconds, 
+		uint _startingDate) public payable;
+
+	function getMyAllowancesCount() public view returns(uint allowancesCount);
+	function getMyAllowanceInfo(uint _index) public 
+		view returns(address sideB, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate);
+	// edit each one 
+	function editMyAllowance(
+		uint _index, 
+		uint _amountWei, 
+		uint _overdraftPpm, 
+		uint _interestRatePpm,
+		uint _periodSeconds, 
+		uint _startingDate) public;
+
+// 3 - overdrafted flag
+	// if you have asked for more than current AllowedAmount and less than AllowedAmount + overdraft
+	function isOverdrafted(address _sideA, address _sideB) public view returns(bool overdrafted);
+	// should be called by the SideA for the SideB
+	function clearOverdraftedFlag(address _sideB) public;
+}
+
+contract ISideB {
+	function getAllowancesCount() public view returns(uint allowancesCount);
+	function getAllowanceInfo(uint _index) public 
+		view returns(address sideA, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate);
+	// only for 'transferrable allowances' that were generated automatically in case of overdraft
+	function transferAllowance(uint _index, address _to) public;
+
+	// will either return money OR 
+	// will return money + generate new allowance (plus interested) to the SideB (me)
+	function charge(uint _index, uint _amountWei) public;
+}
+
+contract IUnderwriterSubsystem {
+	// SideA wants to add current underwriter. This will generate new underwriter request
+	function addUnderwriterRequest(address _underwriter) public;
+	function changeUnderwriter(address _newUnderwriter) public;
+
+	// in order to become an underwriter SideB should accept that
+	function getMyUnderwriterRequestsCount() public view returns(uint count);
+	function getUnderwriterRequest(uint _index) public view returns(address sideB);
+	function acceptUnderwriterRequest(address _sideA, address _sideB) public;
+}
+
--- contracts/Ledger.sol
+++ contracts/Ledger.sol
@@ -1,14 +1,74 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
-contract ISideA {
-// 1 - deposit + withdraw
-	function deposit() public payable;
-	function getDepositBalance() public view returns(uint totalBalanceWei);
-	function withdraw(uint _amountWei) public;
+import "./ILedger.sol";
+import "zeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
-// 2 - allowances that i set to some SideB
-	// this will create _numberOfPeriods allowances
-	// if you have created allowances before for sideB -> throw
+
+contract Ledger is ERC721, ISideA, ISideB {
+	constructor() public {
+	}
+
+	struct UserState {
+		uint currentBalance;
+
+		// TODO: not used right now
+		address currentUnderwriter;	
+
+		// See allowances array below
+		address[] allAllowances;
+	}
+
+	struct Allowance {
+		// by default we set the userState.currentUnderwriter
+		// can be ZERO!
+		address underwriter;
+
+		// if Allowance is generated because of overdraft -> i should be able to trasnfer it 
+		// (sell it or put into collateral)
+		bool transferrable;
+
+		// TODO: transfering ERC721 should update THIS !!!
+		address sideB;
+		uint amountWei;
+		uint overdraftPpm;
+		uint interestRatePpm;
+		uint periodSeconds;
+		uint startingDate;
+
+		address erc721tokenAddress;
+	}
+
+	struct UserToUserState {
+		// TODO - write good comment here...
+		bool isOverdrafted;
+
+		// each allowance is connected with Allowance struct in the allowancesMetainfo
+		address[] allowances;
+	}
+
+	// UserA -> UserState
+	mapping (address=>UserState) userState;
+	// UserA -> UserB -> UserToUserState
+	mapping (address=>mapping(address=>UserToUserState)) user2userState;
+	// ERC721 address -> Allowance
+	mapping (address=>Allowance) allowancesMetainfo;
+
+////////////////////////////////////////////////////////////////////////////////////
+	function deposit() public payable{
+		userState[msg.sender].currentBalance += msg.value;
+	}
+
+	function getDepositBalance() public view returns(uint) {
+		return userState[msg.sender].currentBalance;
+	}
+
+	function withdraw(uint _amountWei) public {
+		require(_amountWei <= getDepositBalance());
+		// TODO: use Safemath here
+		userState[msg.sender].currentBalance-=_amountWei;
+		msg.sender.transfer(_amountWei);
+	}
+
 	function allowAndDeposit(
 		address _sideB, 
 		uint _amountWei, 
@@ -16,11 +76,43 @@ contract ISideA {
 		uint _interestRatePpm, 
 		uint _numberOfPeriods, 
 		uint _periodSeconds, 
-		uint _startingDate) public payable;
+		uint _startingDate) public payable 
+	{
+		// 1 - deposit (can be null!)
+		userState[msg.sender].currentBalance += msg.value;
 
-	function getMyAllowancesCount() public view returns(uint allowancesCount);
+		// 2 - create N allowances ...
+		uint date = _startingDate;
+		for(uint i=0; i<_numberOfPeriods; ++i){
+			_createNewAllowance(_sideB,
+							_amountWei, 
+							_overdraftPpm, 
+							_interestRatePpm, 
+							_periodSeconds, 
+							date);
+
+			date += _periodSeconds;
+		}
+	}
+
+	function getMyAllowancesCount() public view returns(uint){
+		return userState[msg.sender].allAllowances.length;
+	}
+
 	function getMyAllowanceInfo(uint _index) public 
-		view returns(address sideB, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate);
+		view returns(address sideB, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate)
+	{
+		address erc721address = userState[msg.sender].allAllowances[_index];
+		Allowance a = allowancesMetainfo[erc721address];
+
+		sideB = a.sideB;
+		amountWei = a.amountWei;
+		overdraftPpm = a.overdraftPpm;
+		interestRatePpm = a.interestRatePpm;
+		periodSeconds = a.periodSeconds;
+		startingDate = a.startingDate;
+	}
+
 	// edit each one 
 	function editMyAllowance(
 		uint _index, 
@@ -28,40 +120,51 @@ contract ISideA {
 		uint _overdraftPpm, 
 		uint _interestRatePpm,
 		uint _periodSeconds, 
-		uint _startingDate) public;
+		uint _startingDate) public 
+	{
+		// TODO: left for future version
+	}
 
 // 3 - overdrafted flag
 	// if you have asked for more than current AllowedAmount and less than AllowedAmount + overdraft
-	function isOverdrafted(address _sideA, address _sideB) public view returns(bool overdrafted);
-	// should be called by the SideA for the SideB
-	function clearOverdraftedFlag(address _sideB) public;
-}
+	function isOverdrafted(address _sideA, address _sideB) public view returns(bool){
+		return user2userState[_sideA][_sideB].isOverdrafted;
+	}
 
-contract ISideB {
-	function getAllowancesCount() public view returns(uint allowancesCount);
+	// should be called by the SideA for the SideB
+	function clearOverdraftedFlag(address _sideB) public {
+		user2userState[msg.sender][_sideB].isOverdrafted = false;
+	}
+
+// SideB
+	function getAllowancesCount() public view returns(uint allowancesCount){
+		// TODO:
+	}
+
 	function getAllowanceInfo(uint _index) public 
-		view returns(address sideA, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate);
+		view returns(address sideA, uint amountWei, uint overdraftPpm, uint interestRatePpm, uint periodSeconds, uint startingDate){
+		// TODO:
+	}
+
 	// only for 'transferrable allowances' that were generated automatically in case of overdraft
-	function transferAllowance(uint _index, address _to) public;
+	function transferAllowance(uint _index, address _to) public {
+		// TODO:
+	}
 
 	// will either return money OR 
 	// will return money + generate new allowance (plus interested) to the SideB (me)
-	function charge(uint _index, uint _amountWei) public;
-}
+	function charge(uint _index, uint _amountWei) public {
+		// TODO: 
+	}
 
-contract IUnderwriterSubsystem {
-	// SideA wants to add current underwriter. This will generate new underwriter request
-	function addUnderwriterRequest(address _underwriter) public;
-	function changeUnderwriter(address _newUnderwriter) public;
-
-	// in order to become an underwriter SideB should accept that
-	function getMyUnderwriterRequestsCount() public view returns(uint count);
-	function getUnderwriterRequest(uint _index) public view returns(address sideB);
-	function acceptUnderwriterRequest(address _sideA, address _sideB) public;
-}
-
-contract Ledger is ISideA, ISideB, IUnderwriterSubsystem {
-	function Ledger() public {
-
+//////// Internal stuff
+	function _createNewAllowance(address _sideB,
+							uint _amountWei, 
+							uint _overdraftPpm, 
+							uint _interestRatePpm, 
+							uint _periodSeconds, 
+							uint _startingDate) internal 
+	{
+		// TODO:
 	}
 }
